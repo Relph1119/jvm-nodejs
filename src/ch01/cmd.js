cc5090b3c0996582c3a81323110a5d9bfe097f56
Hurf <huruifeng1202@163.com>
2019-10-12 16:29:57 +0800

    完成第一章命令行工具

---
+++ src/ch01/cmd.js
@@ -0,0 +1,25 @@
+/**
+ * @author: HuRuiFeng
+ * @file: cmd.js
+ * @time: 2019/10/12
+ * @desc: 命令行类
+ */
+
+class Cmd{
+    constructor(program){
+        this.cpOption = program.classpath;
+        this.class_name = program.args[0];
+        this.argvs = program.args.slice(1);
+        this.version = program.version();
+    }
+
+    print_classpath(){
+        console.log('classpath:%j class:%j args:%j\n', this.cpOption, this.class_name, this.argvs)
+    }
+
+    print_args(){
+        return '[' + this.argvs.join(' ') + ']';
+    }
+}
+
+module.exports = Cmd;
---
+++ src/develop_code/main.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+/**
+ * @author: HuRuiFeng
+ * @file: main.js
+ * @time: 2019/10/10
+ * @desc:
+ */
+let program = require('commander');
+let Cmd = require('./cmd');
+
+function main(input_args){
+    process.argv = input_args;
+    program
+        .version('0.0.1')
+        .usage('[options] class [args...]')
+        .option('-c, --classpath [value]', 'Class Path')
+        .parse(process.argv);
+
+    if(input_args){
+        let cmd = new Cmd(program);
+        if (program.version != null) {
+            start_JVM(cmd);
+        }
+    }
+}
+
+function start_JVM(cmd) {
+    cmd.print_classpath();
+}
+
+let fake_args = ['node', 'main.js', '-h'];
+// let fake_args = ['node', 'main.js', '-V'];
+// let fake_args = ['node', 'main.js', '-c', 'foo/bar', 'MyApp', 'arg1', 'arg2'];
+main(fake_args);
